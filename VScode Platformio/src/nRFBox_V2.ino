7679e26e2661ac44606cca4667af7d1bbc595ff0
jbohack <joebohack@gmail.com>
2025-05-19 21:26:30 -0400

    Adjusted location of menu item

--- VScode Platformio/src/nRFBox_V2.ino
+++ VScode Platformio/src/nRFBox_V2.ino
@@ -167,10 +167,10 @@ void setup() {
   u8g2.print("by jbohack & zr_crackiin");
 
   u8g2.setFont(u8g2_font_6x10_tf); 
-  int16_t versionWidth = u8g2.getUTF8Width("v2.6.6");
+  int16_t versionWidth = u8g2.getUTF8Width("v2.6.7");
   int16_t versionX = (128 - versionWidth) / 2;
   u8g2.setCursor(versionX, 60);
-  u8g2.print("v2.6.6");
+  u8g2.print("v2.6.7");
   
   u8g2.sendBuffer(); 
   delay(3000);
@@ -245,29 +245,6 @@ if (current_screen == 0 && item_selected == 12) {
     }
   }     
 
-
-if (current_screen == 0 && item_selected == 10) {
-    deauthSetup();
-    while (item_selected == 10) {
-        if (digitalRead(BUTTON_SELECT_PIN) == HIGH) { 
-            deauthLoop();     
-            if (callAbout) {                
-                callAbout = false;  // Toggle the state to not call about()
-            } else {
-                break;  // Toggle the state to break the loop
-                callAbout = true;  // Reset the state for the next cycle
-            }
-
-            while (digitalRead(BUTTON_SELECT_PIN) == HIGH) {
-                // Wait for the button to be released
-                if (callAbout = true){
-                  break;
-                }
-            }
-        }
-    }
-}
-
 if (current_screen == 0 && item_selected == 11) {
     while (item_selected == 11) {
         if (digitalRead(BUTTON_SELECT_PIN) == HIGH) {
@@ -288,6 +265,29 @@ if (current_screen == 0 && item_selected == 11) {
         }
     }
   }
+
+
+  if (current_screen == 0 && item_selected == 10) {
+    deauthSetup();
+    while (item_selected == 10) {
+        if (digitalRead(BUTTON_SELECT_PIN) == HIGH) { 
+            deauthLoop();     
+            if (callAbout) {                
+                callAbout = false;  // Toggle the state to not call about()
+            } else {
+                break;  // Toggle the state to break the loop
+                callAbout = true;  // Reset the state for the next cycle
+            }
+
+            while (digitalRead(BUTTON_SELECT_PIN) == HIGH) {
+                // Wait for the button to be released
+                if (callAbout = true){
+                  break;
+                }
+            }
+        }
+    }
+}
 
 
 if (current_screen == 0 && item_selected == 9) {
